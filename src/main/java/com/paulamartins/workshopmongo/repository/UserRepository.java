af43328aa36c11da677d09a03ac4783e88c577aa
PaulaMartinsGarcia <paulastsmartins@globo.com>
2021-06-30 21:03:00 -0300

    projeto completo

---
+++ src/main/java/com/paulamartins/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.paulamartins.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.paulamartins.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
